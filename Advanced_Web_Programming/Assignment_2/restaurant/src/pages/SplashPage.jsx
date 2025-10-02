21e1c317adb64cf3561bc7f5b92ea00b24c44afe
Adam Gaines <adam.j.gaines.mil@swf.army.mil>
2025-10-02 11:45:51 -0500

    Updated Splash Page

--- Advanced_Web_Programming/Assignment_2/restaurant/src/pages/SplashPage.jsx
+++ Advanced_Web_Programming/Assignment_2/restaurant/src/pages/SplashPage.jsx
@@ -1,23 +1,20 @@
+import grandpa_richard from '../assets/grandpa_richard.png'
 const SplashPage = () => {
 
     return (
         <>
-            <div className="row border-cream" style={{height: '100px'}}>
+            <div className="row" style={{height: '100px'}}>
 
-                <div className="col-lg-4 col-sm-4" style={{backgroundColor: 'var(--primary-black)'}}>
+                <div className="col-lg-4 col-sm-4">
                     <img
-                        src="../assets/grandpa_richard.png"
+                        src= {grandpa_richard}
                         alt="Grandpa Richard and his Prized Pickle"
-                        style={{
-                            width: '100%',
-                            marginTop: '50px',
-                            border: 'var(--background-cream) solid 3px',
-                            borderRadius: '50%'
-                        }}
+                        className="Founder-Image"
+
                     />
                 </div>
 
-                <div className="bebas-neue-regular col-lg-8 col-sm-8">
+                <div className="bebas-neue-regular col-lg-8 col-sm-8 mt-3">
                     <p>
                         Richard's pickles strives to bring a unique addition to the restaurants in our local area: A
                         Pickle Entree. Healthy, lean, and wildly tasteful, Richard's Pickles are a delightful change to
