1da8fee01198b9d34c4d463074f764bef7d3f727
Adam Gaines <adam.j.gaines.mil@swf.army.mil>
2025-11-03 13:13:04 -0600

    Saving progress.

--- Intermediate_Web_Programming/Capstone_2/Frontend/src/components/Widgets.tsx
+++ Intermediate_Web_Programming/Capstone_2/Frontend/src/components/Widgets.tsx
@@ -1,6 +1,7 @@
 import {getWidgets} from "../util/WidgetClient.ts";
 import {useEffect, useState} from "react";
 import type {Widget} from "../util/Widget.ts";
+import wimsicalWidget from "../assets/Whimsical-Wobbler.jpeg";
 
 export const Widgets= () => {
 
@@ -15,14 +16,19 @@ export const Widgets= () => {
     const widgetsTabled = widgets.map((element, index) => {
         return(
             <div className="w-full rounded overflow-hidden shadow-lg" id={'widget'} key = {index}>
-                <img src={element.imageURL} alt={element.name + " image"} />
+                <img src={wimsicalWidget} alt={element.name + " image"} />
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{element.name}</div>
                     <p className="text-gray-700 text-base"> {element.description} </p>
                 </div>
-                    {element.price}
+
                 <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{element.rating}</span>
+                    <label htmlFor=""> Price <br/>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">  {element.price}</span>
+                    </label>
+                    <label htmlFor=""> Rating <br/>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">  {element.rating}</span>
+                    </label>
                 </div>
             </div>
         )
